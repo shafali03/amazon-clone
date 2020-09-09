cf4a1faad28755ffe718b18fa942ae029285a5c8
shafali03 <shaf03sm@gmail.com>
2020-09-09 13:04:51 +0100

    Created checkout route path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <h1>Checkout out component</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
