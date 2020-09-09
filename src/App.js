dc3136ab1aaa2a46df6d75d1be12f8d70a15c388
shafali03 <shaf03sm@gmail.com>
2020-09-09 13:28:44 +0100

    Added a checkout banner image

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Checkout from './components/Checkout/Checkout';
+
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path='/checkout'>
-            <Header />
-            <h1>Checkout out component</h1>
+            <Checkout />
           </Route>
           <Route path='/'>
-            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Checkout.css'
+
+function Checkout() {
+  return (
+    <div className='checkout'>
+      <div className="checkout__left">
+        <img className="checkout__ad" src="https://res.cloudinary.com/shafali/image/upload/v1599654332/check-banner_twp95q.jpg" alt="checkout-banner" />
+      </div>
+
+    </div>
+  )
+}
+
+export default Checkout
