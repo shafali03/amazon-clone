20dc61d32923520cf80b5e53d4fc324203f6ae64
shafali03 <shaf03sm@gmail.com>
2020-09-10 12:19:03 +0100

    Added functionality to add and display product details on the checkout page

---
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './CheckoutProduct.css'
+
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+
+
+  const removeFromBasket = () => {
+
+  }
+
+  return (
+    <div className='checkoutProduct'>
+      <img className='checkoutProduct__image' src={image} alt="cart-items-images" />
+
+      <div className='checkoutProduct__info'>
+        <p className='checkoutProduct__title'>{title}</p>
+        <p className="checkoutProduct__price">
+          <small>£</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
+        </div>
+        <button>Remove from Basket</button>
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
