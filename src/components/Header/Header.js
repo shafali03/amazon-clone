ec76b6589a09a17883a762297c275d406741519f
shafali03 <shaf03sm@gmail.com>
2020-09-11 09:58:41 +0100

    Displays user email when user is logged in home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -33,7 +33,7 @@ function Header() {
         <Link to={!user && '/login'}>
           <div onClick={handleAuthentication} className="header__option">
             <span
-              className='header__optionLinkOne'>Hello Guest</span>
+              className='header__optionLinkOne'>Hello, {user?.email}</span>
             <span
               className='header__optionLinkTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
