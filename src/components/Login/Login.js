d6cd15e4ea1981b3fe02b9431a6f95c6f935611e
shafali03 <shaf03sm@gmail.com>
2020-09-11 08:40:37 +0100

    Added functionality for register and create a new users

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { auth } from '../../firebase'
 
 function Login() {
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = e => {
+    e.preventDefault()
+
+
+  }
+
+  // Create a new user with email and password
+  const register = e => {
+    e.preventDefault()
+
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+
+        console.log(auth)
+        if (auth) {
+          history.push('/')
+        }
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
     <div className='login'>
       <Link to='/'>
-        <img className='login__logo'
-          src="https://res.cloudinary.com/shafali/image/upload/v1599743299/amazon-logo-login-removebg-preview_wnzie1.png" alt="" />
+        <img
+          className='login__logo'
+          src="https://res.cloudinary.com/shafali/image/upload/v1599743299/amazon-logo-login-removebg-preview_wnzie1.png" alt="sign in logo" />
       </Link>
 
       <div className="login__container">
@@ -15,16 +42,32 @@ function Login() {
 
         <form action="">
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
-          <button className='login__signInButton'>Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button
+            type='submit'
+            onClick={signIn}
+            className='login__signInButton'>Sign In
+          </button>
         </form>
+
         <p>
           By signing-in you agree to AMAZON CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
         </p>
-        <button className='login__registerButton'>Create your Amazon Account</button>
+
+        <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+
       </div>
     </div>
   )
